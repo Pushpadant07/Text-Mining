ea01a1f3ebb51612eca0196c4e7a49bfbcc0f20f
Pushpadant M Gobaje <66263773+Pushpadant07@users.noreply.github.com>
2020-10-12 12:11:53 +0530

    Add files via upload

---
+++ sentimental analysis for Movie-GOT.py
@@ -0,0 +1,120 @@
+
+import requests   # Importing requests to extract content from a url
+from bs4 import BeautifulSoup as bs # Beautifulsoup is for web scrapping...used to scrap specific content 
+import re # regular expressions 
+
+# import nltk
+# from nltk.corpus import stopwords - "If we dont have the stopwords in our system then we have to import this module" 
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# creating empty reviews list 
+GOT_reviews =[]
+#forest = ["the","king","of","jungle"]
+
+for i in range(1,20):
+  ip=[] # https://www.imdb.com/title/tt0944947/reviews?ref_=tt_urv
+ 
+  url = "https://www.imdb.com/title/tt0944947/reviews?ref_=tt_urv"+str(i)
+  response = requests.get(url)
+  soup = bs(response.content,"html.parser")# creating soup object to iterate over the extracted content 
+  reviews = soup.findAll("div",attrs={"class","text show-more__control"})
+  # Extracting the content under specific tags  
+  
+  for i in range(len(reviews)):
+    ip.append(reviews[i].text)  
+  GOT_reviews=GOT_reviews+ip  # adding the reviews of one page to empty list which in future contains all the reviews
+help(WordCloud)  
+# writng reviews in a text file 
+#with open("iphone.txt","w",encoding='utf8') as output:
+#    for i in iphone_reviews:
+#        output.write(i+"\n\n")
+#    #output.write(str(iphone_reviews))
+
+
+
+    
+# Joinining all the reviews into single paragraph 
+# ip_rev_string = " ".join(iphone_reviews)
+ip_rev_string = " ".join(GOT_reviews)
+
+
+# Removing unwanted symbols incase if exists
+ip_rev_string = re.sub("[^A-Za-z" "]+"," ",ip_rev_string).lower()#convertiong to lower case
+ip_rev_string = re.sub("[0-9" "]+"," ",ip_rev_string)#removing the numbers 0-9 anywhere in my code 
+
+
+
+# words that contained in iphone 7 reviews
+ip_reviews_words = ip_rev_string.split(" ")
+
+#stop_words = stopwords.words('english')
+
+with open("D:\\ExcelR Data\\Assignments\\Text Mining\\stop.txt","r") as sw:
+    stopwords = sw.read()
+
+stopwords = stopwords.split("\n")
+
+#stp_wrds = stopwords+stop_words
+
+# temp = ["this","is","awsome","Data","Science"]
+# [i for i in temp if i not in "is"]
+#     output = ['this', 'awsome', 'Data', 'Science'] - 'is' removed from the line 
+
+ip_reviews_words = [w for w in ip_reviews_words if not w in stopwords]
+
+
+
+# Joinining all the reviews into single paragraph 
+ip_rev_string = " ".join(ip_reviews_words)
+
+# WordCloud can be performed on the string inputs. That is the reason we have combined 
+# entire reviews into single paragraph
+# Simple word cloud
+
+
+wordcloud_ip = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_rev_string)
+
+plt.imshow(wordcloud_ip)
+
+# positive words # Choose the path for +ve words stored in system
+with open("D:\\ExcelR Data\\Assignments\\Text Mining\\positive-words.txt","r") as pos:
+  poswords = pos.read().split("\n")
+  
+poswords = poswords[36:]
+
+
+
+# negative words  Choose path for -ve words stored in system
+with open("D:\\ExcelR Data\\Assignments\\Text Mining\\negative-words.txt","r") as neg:
+  negwords = neg.read().split("\n")
+
+negwords = negwords[37:]
+
+# negative word cloud
+# Choosing the only words which are present in negwords
+ip_neg_in_neg = " ".join ([w for w in ip_reviews_words if w in negwords])
+
+wordcloud_neg_in_neg = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_neg_in_neg)
+
+plt.imshow(wordcloud_neg_in_neg)
+
+# Positive word cloud
+# Choosing the only words which are present in positive words
+ip_pos_in_pos = " ".join ([w for w in ip_reviews_words if w in poswords])
+wordcloud_pos_in_pos = WordCloud(
+                      background_color='black',
+                      width=1800,
+                      height=1400
+                     ).generate(ip_pos_in_pos)
+
+plt.imshow(wordcloud_pos_in_pos)
---
+++ twitter for RDJ.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import tweepy 
+
+Consumer_key='kwBrATHHoCSOdkyiya8Y89fdl'
+Consumer_secret='ru4zYgSfEtpj7WdlRw3SFRcEPICgGSSKjlCPlSqIVXUlfipmuu'
+Access_key='813308482342490112-vDWTOq0sbMb7GF5IfC9qtfGGSsk1eJm'
+Access_secret='hMKJwymG0jG0rXYZuCXoSHApTHseVtZKdNeJA3CPAohth'
+alltweets = []	
+def get_all_tweets(screen_name):
+    auth = tweepy.OAuthHandler(Consumer_key,Consumer_secret)
+    auth.set_access_token(Access_key,Access_secret)
+    api = tweepy.API(auth)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    alltweets.extend(new_tweets)
+    
+    oldest = alltweets[-1].id - 1
+    while len(new_tweets)>0:
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest) #save most recent tweets
+        alltweets.extend(new_tweets) #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+        
+    outtweets = [[tweet.created_at,tweet.entities["hashtags"],tweet.entities["user_mentions"],tweet.favorite_count,
+                  tweet.geo,tweet.id_str,tweet.lang,tweet.place,tweet.retweet_count,tweet.retweeted,tweet.source,tweet.text,
+                  tweet._json["user"]["location"],tweet._json["user"]["name"],tweet._json["user"]["time_zone"],
+                  tweet._json["user"]["utc_offset"]] for tweet in alltweets]
+    
+    
+    tweets_df = pd.DataFrame(columns = ["time","hashtags","user_mentions","favorite_count",
+                                    "geo","id_str","lang","place","retweet_count","retweeted","source",
+                                    "text","location","name","time_zone","utc_offset"])
+    tweets_df["time"]  = pd.Series([str(i[0]) for i in outtweets])
+    tweets_df["hashtags"] = pd.Series([str(i[1]) for i in outtweets])
+    tweets_df["user_mentions"] = pd.Series([str(i[2]) for i in outtweets])
+    tweets_df["favorite_count"] = pd.Series([str(i[3]) for i in outtweets])
+    tweets_df["geo"] = pd.Series([str(i[4]) for i in outtweets])
+    tweets_df["id_str"] = pd.Series([str(i[5]) for i in outtweets])
+    tweets_df["lang"] = pd.Series([str(i[6]) for i in outtweets])
+    tweets_df["place"] = pd.Series([str(i[7]) for i in outtweets])
+    tweets_df["retweet_count"] = pd.Series([str(i[8]) for i in outtweets])
+    tweets_df["retweeted"] = pd.Series([str(i[9]) for i in outtweets])
+    tweets_df["source"] = pd.Series([str(i[10]) for i in outtweets])
+    tweets_df["text"] = pd.Series([str(i[11]) for i in outtweets])
+    tweets_df["location"] = pd.Series([str(i[12]) for i in outtweets])
+    tweets_df["name"] = pd.Series([str(i[13]) for i in outtweets])
+    tweets_df["time_zone"] = pd.Series([str(i[14]) for i in outtweets])
+    tweets_df["utc_offset"] = pd.Series([str(i[15]) for i in outtweets])
+    tweets_df.to_csv(screen_name+"_tweets.csv")
+    return tweets_df
+
+tweet = get_all_tweets("RobertDowneyJr")
